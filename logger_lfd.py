7f8590848ad5aa3d8b4bb1f7a96c47241c4ea538
Kevin Wells <kwells@icloud.com>
2017-10-20 03:09:03 +0000

    Add logging file

---
+++ logger_lfd.py
@@ -0,0 +1,21 @@
+import logging
+from chroma.log import logger as chroma_logger
+
+logging.basicConfig(format='[%(asctime)s] logging.BASIC_FORMAT', datefmt='%a, %d %b %Y %H:%M:%S') # Avoid "no handlers could be found" warnings
+logger = logging.getLogger('LFD')
+
+# Seems like there should be a simpler way...
+# See: https://docs.python.org/2/howto/logging-cookbook.html
+ch = logging.StreamHandler()   # Console logging
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('[%(asctime)s] ' + logging.BASIC_FORMAT, "%Y-%m-%d %H:%M:%S")
+ch.setFormatter(formatter)
+# add the handlers to logger
+logger.addHandler(ch)
+logger.propagate = False
+
+logger.setLevel(logging.DEBUG)
+
+chroma_logger.setLevel(logging.DEBUG)
+
